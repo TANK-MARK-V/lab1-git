cac77e63da88f7c2359c6a8316c272ea1832e606
TANK-MARK-V <dvornikov.mark@mail.ru>
2026-09-06 20:25:19 +0700

    Язык изменён на английский

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Лабораторная работа № 1. Вариант 40.
 // Выполнил: Дворников М.С., группа ПИ-54.
 #include <iostream>
-#include <windows.h>
 using namespace std;
 
 // ===== Расчётные функции варианта =====
@@ -19,42 +18,40 @@ double waveSpeed(double lambda, double T) {
 
 // ===== Главная функция: меню =====
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
     int choice;
     double v, f, lambda, T;
     do {
-        cout << "\n=== Вариант 40: Калькулятор расчётов для волн ===\n";
-        cout << "1. Длина волны\n";
-        cout << "2. Скорость волны\n";
-        cout << "0. Выход\n";
-        cout << "Выберите пункт: ";
+        cout << "\n=== Variant 40: Waves calculator ===\n";
+        cout << "1. Wave length\n";
+        cout << "2. Wave speed\n";
+        cout << "0. exit\n";
+        cout << "Choice: ";
         cin >> choice;
         switch (choice) {
             case 1:
-                cout << "\nВведите скорость распространения v (м/с): ";
+                cout << "\nEnter v (m/s): ";
                 cin >> v;
-                cout << "Введите частоту f (Гц): ";
+                cout << "Enter f (Hz): ";
                 cin >> f;
-                cout << "\nРЕЗУЛЬТАТ:\n";
-                cout << "Длина волны λ = " << waveLength(v, f) << " м\n";
+                cout << "\nRESULT:\n";
+                cout << "Wave length λ = " << waveLength(v, f) << " m\n";
                 break;
                 
             case 2:
-                cout << "\nВведите длину волны λ (м): ";
+                cout << "\nEnter λ (m): ";
                 cin >> lambda;
-                cout << "Введите период T (с): ";
+                cout << "Enter T (s): ";
                 cin >> T;
-                cout << "\nРЕЗУЛЬТАТ:\n";
-                cout << "Скорость волны v = " << waveSpeed(lambda, T) << " м/с\n";
+                cout << "\nRESLIT:\n";
+                cout << "Wave speed v = " << waveSpeed(lambda, T) << " m/s\n";
                 break;
                 
             case 0:
-                cout << "\nРабота завершена. До свидания!\n";
+                cout << "\nWell done!\n";
                 break;
                 
             default:
-                cout << "\nОшибка: такого пункта нет. Попробуйте снова.\n";
+                cout << "\nError: there is no option with this num. Try again.\n";
         }
     } while (choice != 0);
     return 0;
